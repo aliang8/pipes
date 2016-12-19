86eea8316a6d16d98b1d0c5900b65b2b4c3f7a1d
Anthony Liang <aliang8@stuy.edu>
2016-12-19 15:30:54 -0500

    finishing forking stuff

--- pipe_networking.h
+++ pipe_networking.h
@@ -1,3 +1,4 @@
+#define HANDSHAKE_BUFFER_SIZE 1024
 #define MESSAGE_BUFFER_SIZE 1024
 #define servergreet "Bonjour!" 
 #define clientgreet "Ni hao ma?" // the response
@@ -10,6 +11,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
+
 
 int server_handshake(int *);
 int client_handshake(int *);
+int server_handshake1 (char *buffer);
+int server_handshake2 (char *buffer, int from_client);
